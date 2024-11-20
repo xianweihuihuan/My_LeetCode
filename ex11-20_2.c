64d4a3162f1db49f9f94a27d66386054d4c4811d
xianweihuihuan <2907404984@qq.com>
2024-11-20 23:25:55 +0800

    长度最小的子数组

---
+++ ex11-20_2.c
@@ -0,0 +1,27 @@
+int minSubArrayLen(int target, int *nums, int numsSize)
+{
+    int i = 0;
+    int sum = 0;
+    int ans = 100000;
+    int he = 0;
+    for (int k = 0; k < numsSize; k++)
+    {
+        he += nums[k];
+    }
+    if (he < target)
+    {
+        return 0;
+    }
+    for (int j = 0; j < numsSize; j++)
+    {
+        sum += nums[j];
+        while (sum >= target)
+        {
+            int tmp = j - i + 1;
+            ans = (ans < tmp) ? ans : tmp;
+            sum -= nums[i];
+            i++;
+        }
+    }
+    return ans;
+}
