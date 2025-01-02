46a97fc9909ebc40282b903eedc3a26101ca8eb7
xianweihuihuan <2907404984@qq.com>
2025-01-02 11:49:14 +0800

    山脉数组的峰值索引

---
+++ ex1-2_1.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+public:
+    int peakIndexInMountainArray(vector<int> &arr)
+    {
+        int left = 1;
+        int right = arr.size() - 2;
+        while (left < right)
+        {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] > arr[mid + 1])
+            {
+                right = mid;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
